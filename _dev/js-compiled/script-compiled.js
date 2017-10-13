e4a582ce675223c9d049befefb285a17f4a4c0a4
BOSS-PC\boss <contact@eplex.eu>
2017-10-13 15:04:05 +0200

    v1

---
+++ _dev/js-compiled/script-compiled.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$(document).ready(function () {
+
+	var maciek = 'Maciek';
+	var number = 22;
+	for (var i = 0; i < number; i++) {
+		console.log(i);
+	}
+	console.log(maciek);
+});
+//# sourceMappingURL=script-compiled.js.map
---
+++ _dev/js/script.jsx
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	const maciek = 'Maciek';
+	let number = 22;
+	for(let i=0; i<number; i++){
+		console.log(i);
+	}
+	console.log(maciek);
+});
+
